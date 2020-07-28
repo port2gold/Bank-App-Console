64e3fc3d48c20161a653ce252def04c3aa52f90a
Abdulkabir Omotoso <abdulkabir.omotoso@decagon.dev>
2020-07-28 15:37:34 +0100

    program.cs Initial

--- BankApp1/Bank.cs
+++ BankApp1/Bank.cs
@@ -4,26 +4,26 @@ using System.Text;
 
 namespace BankApp1
 {
-    class Bank
+    public class Bank
     {
-        public List<Customer> AllCustomers = new List<Customer>();
+        //public static List<Customer> AllCustomers = new List<Customer>();
         //public void NewCustomer()
         //{
         //    AllCustomers.Add(new Customer());
         //}
 
-        public void GetAllCustomers(List<Customer> customers)
-        {
-            int i = 0;
-            foreach (var item in customers)
-                Console.WriteLine($"{item.CustomerId}{item.CustomerFirstName} {item.CustomerEmail} {item.AllAccount[i].AccountNumber} {item.AllAccount[i].AccountBalance} ");
-            //i = i + 1;
-            i++;
-        }
-        public void RegisterCustomer()
-        {
-            AllCustomers.Add(new Customer(string firstName,string password));
-        }
+        //public void GetAllCustomers(List<Customer> customers)
+        //{
+        //    int i = 0;
+        //    foreach (var item in customers)
+        //        Console.WriteLine($"{item.CustomerId}{item.CustomerFirstName} {item.CustomerEmail} {item.AllAccount[i].AccountNumber} {item.AllAccount[i].AccountBalance} ");
+        //    //i = i + 1;
+        //    i++;
+        //}
+        //public void RegisterCustomer()
+        //{
+        //    AllCustomers.Add(new Customer(string firstName,string password));
+        //}
 
 
         public void LogIn(string firstName, string password)
--- BankApp1/Program.cs
+++ BankApp1/Program.cs
@@ -8,54 +8,87 @@ namespace BankApp1
 {
     class Program
     {
+        enum BankOptions
+        {
+            Register =1,
+            LogIn =2,
+            CloseBankApp = 3
+
+        }
         static void Main(string[] args)
         {
+            Bank myBank = new Bank();
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" );
+            Console.WriteLine("Welcome to the Most Wonderful Bank");
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" );
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"); 
+            Console.WriteLine("What Would you like to do?");
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+            Console.WriteLine("To Register Press ====================>          1");
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+            Console.WriteLine("To Log In Press ======================>          2");
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+            Console.WriteLine("To Exit our wonderful Bank Press ===============> 3");
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+
+//            var Response
+
+
+            //Customer kay = new Customer();
+            //kay.CustomerFirstName = "Lamba";
+            //kay.CustomerLastName = "Lord";
+            //kay.CustomerEmail = "l.lamba@gmail.com";
+            ////Console.WriteLine(kay.CustomerFullName);
+            ////Console.WriteLine(kay.CustomerId);
+            ////Console.WriteLine(kay.CustomerEmail);
+            //kay.CreateAccount(kay.CustomerFullName, DateTime.Now);
+            //Customer customer = new Customer();
+            //customer.CustomerEmail = "kaybeebaba@gmail.com";
+            //customer.CustomerFirstName = "kabir";
+            //customer.CustomerLastName = "Omotoso";
+            //customer.CreateAccount("kabir", DateTime.Now);
+            ////foreach (var item in kay.AllAccount)
+            ////{
+            ////    Console.WriteLine("Customer Id                 Customer Name               Account Number            Account Type              Date Created");
+            ////    Console.WriteLine($"{kay.CustomerId}               {item.AccountOwner}              {item.AccountNumber}             {item.AccountType}              {item.DateCreated}");
+            ////}
+            ////foreach (var item in customer.AllAccount)
+            ////{
+            ////    Console.WriteLine($"{kay.CustomerId}       {item.AccountBalance}      {item.AccountOwner}               {item.AccountNumber}               {item.AccountType}              {item.DateCreated}");
+            ////}
+            //kay.AllAccount[0].Deposit(10000, DateTime.Now, "Money from friends");
+            //kay.AllAccount[0].Deposit(500000, DateTime.Now, "Money for Goods");
+            //kay.AllAccount[0].Deposit(220200, DateTime.Now, "Money for travelling");
+            //kay.CreateAccount(kay.CustomerFirstName, DateTime.Now);
+            //kay.AllAccount[1].Deposit(2000000, DateTime.Now, "Initial Deposit");
+            //kay.AllAccount[1].Deposit(503000, DateTime.Now, "Big Deposit");
+            //kay.AllAccount[1].Deposit(5000, DateTime.Now, "Big Deposit");
+            //kay.AllAccount[1].Withdrawal(1111, DateTime.Now, "Big Deposit");
 
 
 
-            Customer kay = new Customer();
-            kay.CustomerFirstName = "Lamba";
-            kay.CustomerLastName = "Lord";
-            kay.CustomerEmail = "l.lamba@gmail.com";
-            //Console.WriteLine(kay.CustomerFullName);
-            //Console.WriteLine(kay.CustomerId);
-            //Console.WriteLine(kay.CustomerEmail);
-            kay.CreateAccount(kay.CustomerFullName, DateTime.Now);
-            Customer customer = new Customer();
-            customer.CustomerEmail = "kaybeebaba@gmail.com";
-            customer.CustomerFirstName = "kabir";
-            customer.CustomerLastName = "Omotoso";
-            customer.CreateAccount("kabir", DateTime.Now);
-            //foreach (var item in kay.AllAccount)
-            //{
-            //    Console.WriteLine("Customer Id                 Customer Name               Account Number            Account Type              Date Created");
-            //    Console.WriteLine($"{kay.CustomerId}               {item.AccountOwner}              {item.AccountNumber}             {item.AccountType}              {item.DateCreated}");
-            //}
-            //foreach (var item in customer.AllAccount)
-            //{
-            //    Console.WriteLine($"{kay.CustomerId}       {item.AccountBalance}      {item.AccountOwner}               {item.AccountNumber}               {item.AccountType}              {item.DateCreated}");
-            //}
-            kay.AllAccount[0].Deposit(10000, DateTime.Now, "Money from friends");
-            kay.AllAccount[0].Deposit(500000, DateTime.Now, "Money for Goods");
-            kay.AllAccount[0].Deposit(220200, DateTime.Now, "Money for travelling");
-            kay.CreateAccount(kay.CustomerFirstName, DateTime.Now);
-            kay.AllAccount[1].Deposit(2000000, DateTime.Now, "Initial Deposit");
-            kay.AllAccount[1].Deposit(503000, DateTime.Now, "Big Deposit");
-            kay.AllAccount[1].Deposit(5000, DateTime.Now, "Big Deposit");
-            kay.AllAccount[1].Withdrawal(1111, DateTime.Now, "Big Deposit");
-
-
-
-            //foreach (var item in kay.AllAccount[0].AllTransactions)
-            //{
-            //    Console.WriteLine($"{kay.AllAccount[0].AccountOwner} {kay.AllAccount[0].AccountNumber} {kay.AllAccount[0].AccountBalance} {item.Amount} {item.Date} {item.Notes}");
-            //}
-            // Console.WriteLine(Customer.GetAllCustomers(AllCustomers));
-            // Bank.GetAllCustomers(AllCustomers);  
-            kay.AllAccount[1].Withdrawal(2500, DateTime.Now, "Rent");
-            Console.WriteLine($"#{kay.AllAccount[0].Balance()}");
-            //foreach (var item in kay.AllAccount[1].AllTransactions)
-            //{
+            ////foreach (var item in kay.AllAccount[0].AllTransactions)
+            ////{
+            ////    Console.WriteLine($"{kay.AllAccount[0].AccountOwner} {kay.AllAccount[0].AccountNumber} {kay.AllAccount[0].AccountBalance} {item.Amount} {item.Date} {item.Notes}");
+            ////}
+            //// Console.WriteLine(Customer.GetAllCustomers(AllCustomers));
+            //// Bank.GetAllCustomers(AllCustomers);  
+            //kay.AllAccount[1].Withdrawal(2500, DateTime.Now, "Rent");
+            //Console.WriteLine($"#{kay.AllAccount[0].Balance()}");
+            ////foreach (var item in kay.AllAccount[1].AllTransactions)
+            ////{
             //    Console.WriteLine($"{kay.CustomerFirstName} {kay.AllAccount[1].AccountNumber} {kay.AllAccount[1].AccountBalance}");
             //}
         }
